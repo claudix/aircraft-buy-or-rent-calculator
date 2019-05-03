800afa2ff9f29bb7e3b4a44ed5b758dec1b88aec
Claudi Martinez <claudix.kernel@gmail.com>
2019-05-03 21:11:42 +0200

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,27 @@
+(function(window){  
+  var params = {
+    InsuranceCost: {
+      caption: "Yearly insurance cost"      
+    },
+    HangarCost: {
+      caption: "Yearly hangar cost"
+    },
+    Depreciation: {
+      caption: "Yearly depreciation (%)"
+    },
+    LabourMaintenanceCostPer50Hours: {
+      caption: "Maintenance cost per 50 hours (labour)"
+    },
+    MaterialsMaintenanceCostPer50Hours: {
+      caption: "Maintenance cost per 50 hours (materials)"
+    },
+    FuelCostPerHour: {
+      caption: "Fuel cost per hour"
+    },    
+    RentingCost: {
+      caption: "Renting cost per hour"
+    }    
+  }
+
+  window.Calc = {};
+})(window);
